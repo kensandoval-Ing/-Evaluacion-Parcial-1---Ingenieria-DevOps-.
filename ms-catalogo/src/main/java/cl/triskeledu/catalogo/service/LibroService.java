5de988d8eaa3c9bccbd6b6f92856d569aba1b2f4
kensandoval-Ing <ken.sandoval@duocuc.cl>
2026-09-10 22:18:12 -0300

    fix(ms-catalogo): corrige findByAutor mal anidado en LibroService

--- ms-catalogo/src/main/java/cl/triskeledu/catalogo/service/LibroService.java
+++ ms-catalogo/src/main/java/cl/triskeledu/catalogo/service/LibroService.java
@@ -45,17 +45,19 @@ public class LibroService {
     }
 
     public LibroResponse findByIsbn(String isbn) { 
-        public List<LibroResponse> findByAutor(String autor) { return libroMapper.toResponseList(libroRepository.findByAutorContainingIgnoreCase(autor)); }
         return libroMapper.toResponse(getLibroByIsbn(isbn));
     }
 
+    public List<LibroResponse> findByAutor(String autor) {
+    return libroMapper.toResponseList(libroRepository.findByAutorContainingIgnoreCase(autor));
+}
     @Transactional
     public LibroResponse create(LibroRequest request) {
         validateIsbnUnico(request.getIsbn());
         Libro libro = new Libro();  
         libroMapper.updateEntity(request, libro);
         libroRepository.save(libro);
-        sincronizarProyeccion(libro);
+        sincronizarProyeccion(libro); 
         return libroMapper.toResponse(libro);
     }
 
