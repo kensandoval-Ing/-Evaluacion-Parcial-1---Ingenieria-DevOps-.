5e0d001b22a4c2f73de2ca354ea1879fc17b3206
kensandoval-Ing <ken.sandoval@duocuc.cl>
2026-09-10 21:15:54 -0300

    fix(ms-catalogo): corrige import roto en LibroRepository

--- ms-catalogo/src/main/java/cl/triskeledu/catalogo/repository/LibroRepository.java
+++ ms-catalogo/src/main/java/cl/triskeledu/catalogo/repository/LibroRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import cl.triskeledu.catalogo.model.Libro;
 import java.util.Optional;
 
-java.util.Optional;'): import java.util.List;
+import java.util.List;
 
 @Repository
 public interface LibroRepository extends JpaRepository<Libro, Long> {
